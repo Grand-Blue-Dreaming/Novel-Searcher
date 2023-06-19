e79048bd7dedef4d549903742ab452a9ef2fdccb
SamiKhan-cse19 <samikhan@iut-dhaka.edu>
2023-06-19 20:37:15 +0600

    fixed search box interaction, added api call

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -7,10 +7,10 @@ const SearchResults = ({results}) =>
       <h1 className="font-bold">Search Results</h1>
       {results.map((book, index) => (
         <div key={index} className="p-4 border border-gray-300 rounded-md">
-          <h3 className="text-lg font-bold">{book.title}</h3>
-          <p className="text-sm text-gray-500">Author: {book.author}</p>
-          <p className="text-sm text-gray-500">Genre: {book.genre}</p>
-          <p className="mt-2">{book.summary}</p>
+          <h3 className="text-lg font-bold">{book.Title}</h3>
+          <p className="text-sm text-gray-500">Author: {book.Author}</p>
+          <p className="text-sm text-gray-500">Genre: {book.Genre}</p>
+          <p className="mt-2">{book.Summary}</p>
         </div>
       ))}
     </div>
